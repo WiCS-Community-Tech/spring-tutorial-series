706c4bd6a58a11f02630e4b37bfa49b54172adcc
Teal Amore <github@tealamore.com>
2026-03-02 17:16:31 -0500

    first commit

---
+++ src/test/java/edu/emu/wics/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.emu.wics.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
