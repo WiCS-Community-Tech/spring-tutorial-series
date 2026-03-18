31e0c0d8456f1e440345e5032c832f8983d36ff1
Teal Amore <githubs@tealamore.com>
2026-03-18 16:33:32 -0400

    making things a touch easier

--- src/main/java/edu/emu/wics/spring/client/ItemClient.java
+++ src/main/java/edu/emu/wics/spring/client/ItemClient.java
@@ -1,8 +1,13 @@
 package edu.emu.wics.spring.client;
 
 import edu.emu.wics.spring.dto.Item;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 public class ItemClient {
+    // running on localhost:8081
+    // see http://localhost:8081/swagger-ui/index.html for deets
 
     public Item getItem(int id) {
         return new Item(id,
@@ -11,6 +16,11 @@ public class ItemClient {
     }
 
     public Item saveItem(Item item) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Item> request = new HttpEntity<>(item, headers);
+
         return item;
     }
 }
