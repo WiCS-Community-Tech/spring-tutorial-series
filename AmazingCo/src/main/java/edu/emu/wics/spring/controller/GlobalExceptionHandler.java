bc5ae563a20d6c9db0e183c0edcf52cbea399972
Teal Amore <githubs@tealamore.com>
2026-04-22 15:21:15 -0400

    fixed GEH

--- AmazingCo/src/main/java/edu/emu/wics/spring/controller/GlobalExceptionHandler.java
+++ AmazingCo/src/main/java/edu/emu/wics/spring/controller/GlobalExceptionHandler.java
@@ -1,3 +1,15 @@
+package edu.emu.wics.spring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@SuppressWarnings("NullableProblems")
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception exception) {
